0aa8274bd5358c7dc6f99f93c9a1c4a57edd81e2
Yanuar Rizki Aminudin <160224168+YanuarRizkiAminudin@users.noreply.github.com>
2024-02-22 23:47:20 +0700

    Add files via upload

--- SemesterII/Pertemuan2/Buku29.java
+++ SemesterII/Pertemuan2/Buku29.java
@@ -1,21 +1,35 @@
 public class Buku29 {
-        String judul, pengarang;
-        int halaman, stok, harga;
+    String judul, pengarang;
+    int halaman, stok, harga;
 
-        void tampilInformasi(){
-            System.out.println("Judul: " + judul);
-            System.out.println("Pengarang: " + pengarang);
-            System.out.println("Jumlah halman:" + halaman);
-            System.out.println("Sisa stok : " + stok);
-            System.out.println("Harga: Rp " + harga);
-        }
-        void terjual(int jml){
+    void tampilInformasi(){
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halman:" + halaman);
+        System.out.println("Sisa stok : " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+    public Buku29(){
+    }
+    public Buku29(String jd, String pg, int hl, int stok, int hr){
+        judul = jd;
+        pengarang = pg;
+        halaman = hl;
+        this.stok = stok;
+        harga = hr;
+    }
+    void terjual(int jml){
+        if (stok > 0 && stok >= jml){
             stok -= jml;
-        }
-        void restock(int jml){
-            stok += jml;
-        }
-        void gantiHarga(int hrg){
-            harga = hrg;
+            System.out.println("Penjualana berhasil, Sisa stok: " + stok);
+        }else {
+            System.out.println("stok sudah habis.");
         }
     }
+    void restock(int jml){
+        stok += jml;
+    }
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+}
--- SemesterII/Pertemuan2/BukuMain29.java
+++ SemesterII/Pertemuan2/BukuMain29.java
@@ -3,17 +3,22 @@ public class BukuMain29 {
         Buku29 bk1 = new Buku29();
 
         bk1.judul = "Today Ends Tomorrow Comes";
-        bk1.pengarang = "Yanuar Rizki Aminudin";
+        bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
         bk1.stok = 13;
         bk1.harga = 71000;
 
-        System.out.println("Informasi Buku Sebelum Perubahan:");
         bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
-
-        System.out.println("Informasi Buku Setelah Perubahan:");
         bk1.tampilInformasi();
+
+        Buku29 bk2 = new Buku29("Self reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+        Buku29 bk3 = new Buku29("Solo Leveling", "Yanuar Rizki Aminudin", 80, 20, 93000);
+        bk3.tampilInformasi();
     }
 }
+    
