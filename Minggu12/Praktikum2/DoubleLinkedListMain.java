2c738c4d66ef0b995ae74bc656edf2e1ca9d16fd
Yanuar Rizki Aminudin <160224168+YanuarRizkiAminudin@users.noreply.github.com>
2024-05-19 11:55:01 +0700

    Add files via upload
    Minggu12

---
+++ Minggu12/Praktikum2/DoubleLinkedListMain.java
@@ -0,0 +1,26 @@
+package Praktikum2;
+/**...4 lines */
+public class DoubleLinkedListMain {
+    public static void main(String[] args) throws Exception {
+        DoubleLinkedLists dll = new DoubleLinkedLists();
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+    }
+}
---
+++ Minggu12/doublelinkedlists/Node28.java
@@ -0,0 +1,12 @@
+package doublelinkedlists;
+/**...4 lines */
+public class Node28{
+    int data;
+    Node28 prev, next;
+
+    Node28(Node28 prev, int data, Node28 next){
+        this.prev=prev;
+        this.data=data;
+        this.next= next;
+    }
+}
